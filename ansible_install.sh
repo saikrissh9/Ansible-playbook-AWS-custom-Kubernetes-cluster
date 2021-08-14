b85c532b72d9b9f060a344c91bdee645d25965cf
saikrissh9 <40247560+saikrissh9@users.noreply.github.com>
2021-08-13 19:56:29 -0700

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -4,3 +4,8 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
+sudo apt-get install -y git
+
+
+
+
